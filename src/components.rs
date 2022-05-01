5cb3205b2c510046916112761f2cf1ff0b1fb849
will <64425242+willser@users.noreply.github.com>
2022-05-01 13:04:46 +0800

    style(http): Better style,I think

---
+++ src/components.rs
@@ -0,0 +1,10 @@
+use crate::egui::Response;
+use eframe::egui::{Rect, Ui, Vec2, Widget};
+
+pub fn widget_with_size(ui: &mut Ui, size: Vec2, widget: impl Widget) -> Response {
+    let widget_rect = Rect::from_min_size(
+        ui.min_rect().max + Vec2::new(5.0, ui.min_rect().min.y - ui.min_rect().max.y),
+        size,
+    );
+    ui.put(widget_rect, widget)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod color;
+mod components;
 mod request;
 mod setting;
 
