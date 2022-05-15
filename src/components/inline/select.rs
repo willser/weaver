ab563962ad0a01739499913ec3647589f755f7d3
will <64425242+willser@users.noreply.github.com>
2022-05-15 21:29:23 +0800

    feat(components): Add init code about select

--- src/components/inline/select.rs
+++ src/components/inline/select.rs
@@ -1,28 +1,33 @@
+use crate::color;
+use crate::egui::Rounding;
+use crate::style::get_widgets;
 use eframe::egui::style::{Margin, Widgets};
 use eframe::egui::{
-    pos2, FontSelection, Id, Label, Rect, Response, Sense, TextStyle, Ui, Vec2, WidgetInfo,
+    popup, pos2, FontSelection, Id, Label, Rect, Response, Sense, TextStyle, Ui, Vec2, WidgetInfo,
     WidgetText, WidgetType,
 };
 use eframe::epaint;
-use eframe::epaint::Shape;
+use eframe::epaint::{RectShape, Shape, Stroke};
 
-pub struct Select {
+pub struct Select<'t> {
     // Widgets of element
+    pub(crate) value: &'t mut WidgetText,
     pub widgets: Widgets,
     pub outer_margin: Option<Margin>,
     pub inner_margin: Option<Margin>,
     pub id_source: Id,
     pub label: Option<WidgetText>,
     pub selected_text: WidgetText,
-    pub option: Option<WidgetText>,
+    pub option: Option<Vec<WidgetText>>,
     pub width: Option<f32>,
     pub height: Option<f32>,
     pub override_text_size: Option<f32>,
 }
 
-impl Select {
-    pub fn new(id: impl Into<Id>) -> Self {
+impl<'t> Select<'t> {
+    pub fn new(id: impl Into<Id>, value: &'t mut WidgetText) -> Self {
         Self {
+            value,
             widgets: Default::default(),
             outer_margin: Default::default(),
             inner_margin: Default::default(),
@@ -36,8 +41,9 @@ impl Select {
         }
     }
 
-    pub fn show(&self, ui: &mut Ui) -> Response {
+    pub fn show(self, ui: &mut Ui) {
         let Self {
+            value,
             widgets,
             outer_margin,
             inner_margin,
@@ -50,34 +56,19 @@ impl Select {
             override_text_size,
         } = self;
 
-        let button_id = ui.make_persistent_id(id_source);
-
-        let outer_margin = if let Some(margin) = outer_margin {
-            *margin
-        } else {
-            Margin::same(0.0)
-        };
+        let popup_id = id_source.with("popup");
+        let where_to_put_background = ui.painter().add(Shape::Noop);
         let inner_margin = if let Some(margin) = inner_margin {
-            *margin
+            margin
         } else {
             Margin::same(0.0)
-        };
-
-        let row_height = if let Some(text_size) = override_text_size {
-            *text_size
-        } else {
-            let id = FontSelection::default().resolve(ui.style());
-            ui.fonts().row_height(&id)
         };
 
         let wrap_width = ui.available_width();
 
-        let text = match label {
-            None => WidgetText::from(""),
-            Some(text) => text.clone(),
-        };
-
-        let text = text.into_galley(ui, None, wrap_width, TextStyle::Body);
+        let text = value
+            .clone()
+            .into_galley(ui, None, wrap_width, TextStyle::Body);
 
         let mut desired_size = text.size() + inner_margin.sum();
 
@@ -90,17 +81,50 @@ impl Select {
             },
         );
 
-        response.widget_info(|| WidgetInfo::labeled(WidgetType::Button, text.text()));
+        response.widget_info(|| WidgetInfo::labeled(WidgetType::Button, value.text()));
 
         if ui.is_rect_visible(rect) {
             let visuals = ui.style().interact(&response);
             let text_pos = ui
                 .layout()
-                .align_size_within_rect(text.size(), rect.expand2(inner_margin.sum()))
+                .align_size_within_rect(text.size(), rect.shrink2(inner_margin.left_top()))
                 .min;
 
             text.paint_with_visuals(ui.painter(), text_pos, visuals);
+
+            let response = ui.interact(rect, id_source, Sense::click());
+
+            let visuals = ui.style().interact(&response);
+            ui.painter().set(
+                where_to_put_background,
+                RectShape {
+                    rect,
+                    rounding: visuals.rounding,
+                    fill: visuals.bg_fill,
+                    stroke: visuals.bg_stroke,
+                },
+            );
+
+            if response.clicked() {
+                println!("{:?}", ui.next_widget_position());
+                println!("{:?}", rect);
+                println!("lasted : {:?}", text_pos);
+                ui.memory().toggle_popup(popup_id);
+            }
+            let inner = popup::popup_below_widget(ui, popup_id, &response, |ui| match option {
+                None => {
+                    ui.label("No option");
+                }
+                Some(options) => {
+                    for x in options {
+                        let mut response = ui.selectable_label(value.text() == x.text(), x.text());
+                        if response.clicked() {
+                            *value = x;
+                            response.mark_changed();
+                        }
+                    }
+                }
+            });
         }
-        response
     }
 }
